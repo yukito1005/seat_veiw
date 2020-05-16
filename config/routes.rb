9ee05d1064637971734cebbc7b623692dee41a2b
yukito1005 <tokuoka.yukito@gmail.com>
2020-05-16 10:17:27 +0900

    syuuseir
    dd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get 'homes/index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'posts#index'
+  root to:'homes#index'
   resources :posts ,only: [:index, :new, :create]
 
 end
